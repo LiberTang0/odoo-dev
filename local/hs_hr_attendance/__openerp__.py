978db1eb0e307555235ded0b7aa475b784bae050
guoyufeng <guo_yufeng@outlook.com>
2016-10-14 16:55:43 +0800

    create hs_hr_attendance module

---
+++ local/hs_hr_attendance/__openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Employee Attendance',
+    'summary': 'Manage employee shifts, attendance, overtime and leave etc.',
+    'description': 'To be described',
+    'author': 'Guo Yufeng',
+    'category': 'Hengshen HR',
+    'version': '1.0.0',
+    'depends': ['base'],
+    'data': []
+}
